c441f7e0872a90704d600ede054e71f03b2d8743
Wisdom-wisely-Samson <wiselywisdom4@gmail.com>
2026-03-10 08:07:44 +0300

    authMiddleware updated

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-const User = require("../models/user");
+const User = require("../models/User");
 // authMiddleware.js
 exports.protect = (roles = []) => {
   return async (req, res, next) => {
